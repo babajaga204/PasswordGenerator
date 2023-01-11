76118366145ac48a7d8bbcff78916fb2935292df
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-11 11:26:48 +0100

    Argument validation finished. Started pwd generation

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -11,9 +11,72 @@ namespace PasswordGenerator
                 ShowOptions();
                 return;
             }
-           
+
+
+            string pattern = args[1].PadRight(Convert.ToInt32(args[0]), 'l');
+            Console.WriteLine(pattern);
+
         }
 
+        static bool IsValid(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return false;
+            }
+            else if (args.Length == 2)
+            {
+                CheckArgs(args);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void CheckArgs(string[] args)
+        {
+            if (!CheckFirstArg(args[0])) { Console.WriteLine("Please enter a valid number"); ShowOptions(); }
+            else if (!CheckSecondArg(args[1])) { Console.WriteLine("Please insert valid options"); ShowOptions(); }
+        }
+
+        private static bool CheckFirstArg(string firstArg)
+        {
+            foreach (var c in firstArg)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+        private static bool CheckSecondArg(string secondArg)
+        {
+            if (secondArg.Length == 0) { return false; }
+            foreach (var c in secondArg)
+            {
+                if (!CheckChar(c))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        private static bool CheckChar(char c)
+        {
+            if (c == 'l' || c == 'L' || c == 'd' || c == 's') { return true; }
+            else { return false; }
+        }
         static void ShowOptions()
         {
             Console.WriteLine("PasswordGenerator");
@@ -31,75 +94,6 @@ namespace PasswordGenerator
             Console.WriteLine("     Min. 1 upper case");
             Console.WriteLine("     Min. 2 special characters");
             Console.WriteLine("     Min. 2 digits");
-        }
-
-        static bool IsValid(string[] args)
-        {
-            if (args.Length == 0)
-            {
-                return false;
-            }
-            else
-            {
-                CheckArgs(args);
-                return true;
-            }
-        }
-        static void CheckArgs(string[] args)
-        {
-            CheckFirstArg(args[0]);
-            CheckSecondArg(args[1]);
-        }
-
-        private static void CheckFirstArg(string firstArg)
-        {
-            foreach (var c in firstArg)
-            {
-                if (!char.IsDigit(c))
-                {
-                    Console.WriteLine("Please enter a number");
-                    ShowOptions();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("We did it! test");
-                    //TODO: Convert string digits to int and use for Password Length Argument
-                }
-            }
-        }
-        private static void CheckSecondArg(string secondArg)
-        {
-            foreach (var c in secondArg)
-            {
-                CheckChar(c);
-            }
-        }
-        private static bool CheckChar(char c)
-        {
-            if (c == 'l')
-            {
-                Console.WriteLine(c);
-                return true;
-            }
-            else if (c == 'L')
-            {
-                Console.WriteLine(c);
-                return true;
-            }
-            else if (c == 'd')
-            {
-                Console.WriteLine(c);
-                return true;
-            }
-            else if (c == 's')
-            {
-                Console.WriteLine(c);
-                return true;
-            }
-            Console.WriteLine("Please insert valid options");
-            ShowOptions();
-            return false;
         }
     }
 }
